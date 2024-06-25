22e8a9d33bebcdef8b83adffff4667c98c7c6388
ghazza-dev <beedeeboy18@gmail.com>
2024-06-25 18:21:26 +1000

    Update board.py
    The script now determines how many bombs are neighbouring each cell and prints it in the terminal

--- board.py
+++ board.py
@@ -6,9 +6,6 @@ class Cell:
         self.flagged=False
         self.neighbourBombs=0
 
-    def determineNeighbours(self):
-        pass
-
 class Bomb(Cell):
     def __init__(self, row, col):
         super().__init__(row, col)
@@ -18,19 +15,26 @@ class Bomb(Cell):
 class Board:
     def __init__(self, size=[]):
         self.size=size
+        
+        self.bombIndexes=[]   # array containing the coordinates of each bomb
+        self.cellIndexes=[]   # array containing the coordinates of each cell
+
+        self.cells=[]         # array containing each cell as an object of the 'Cell' class
+
+        self.chooseMines()
         self.createMines()
         self.printBoard()
+        self.assignCellHint()
 
-    def createMines(self):
-        num_of_mines=round((self.size[0]*self.size[1]) // 4)            # determine the number of mines in the board (amount of cells divided by x)
+    def chooseMines(self):  # choose random coordinates to be bombs
+        num_of_mines=round((self.size[0]*self.size[1]) // 4)            # determine the number of mines in the board (amount of cells divided by 4)
         print(num_of_mines)
         import random
-        self.bombIndexes=[]                                     # list containing the coordinates of each bomb 
         
-        for i in range(num_of_mines):
-            randrow=random.randint(0,self.size[0]-1)        
-            randcol=random.randint(0,self.size[1]-1)                # choose a random row and col for each bomb to be placed
-            while [randrow,randcol] in self.bombIndexes:                 # and add them to an array 'bombIndexes'
+        for i in range(num_of_mines):       # choose a random row and col for each bomb to be placed, and add them to the array 'bombIndexes'
+            randrow=random.randint(0,self.size[0]-1)
+            randcol=random.randint(0,self.size[1]-1) 
+            while [randrow,randcol] in self.bombIndexes:  # check if the randomly chosen coordinate has already been chosen                  
                 randrow=random.randint(0,self.size[0]-1)       
                 randcol=random.randint(0,self.size[1]-1) 
             self.bombIndexes.append([randrow,randcol])
@@ -38,8 +42,8 @@ class Board:
         print(self.bombIndexes)
         print()
 
-        # loop through every cell in the grid and determine whether it is a bomb or not 
-        
+    def createMines(self):    # loop through every cell in the grid and determine whether it is a bomb or not 
+
         self.grid=[]
 
         for row in range(self.size[0]):
@@ -47,13 +51,18 @@ class Board:
             for col in range(self.size[1]):
                 current=[row,col]
                 if current in self.bombIndexes:
-                    current=Bomb(row,col)
+                    current=Bomb(row,col)              # if the current coordinate is seen to be a bomb, add it to the 'bomb' class 
                 else:
-                    current=Cell(row,col)
+                    self.cellIndexes.append([row,col])
+                    current=Cell(row,col)              # if not, add it to the 'cell' class
+                    self.cells.append(current)
                 rowArray.append(col)
             self.grid.append(rowArray)
 
-    def printBoard(self):
+        print(self.cellIndexes)
+        print()
+
+    def printBoard(self):  # print the grid in the terminal
 
         for row in range(0, len(self.grid)):
             s=""
@@ -65,6 +74,36 @@ class Board:
                     s=s+f"({row},{col}) "
             print(s)
         print()
-        
-                 
-board=Board([5,10]) 
+
+    def assignCellHint(self):   # loop through every cell and call 'determineNeighbours' function. Assign the returned value to cell.neighbourBombs
+        for cell in self.cells:
+            cell.neighbourBombs=self.determineNeighbours(cell.row,cell.col)
+            print(f"({cell.row},{cell.col}) has {cell.neighbourBombs} neighbour bombs")
+            
+    def determineNeighbours(self, row, col):
+        neighbours=0
+        if (row-1 >= 0) and ([row-1,col] in self.bombIndexes):    # check above
+            print(f'[{row},{col}] has a bomb above')
+            neighbours+=1
+        if (row+1 <= len(self.grid)) and ([row+1,col] in self.bombIndexes): # check below
+            print(f'[{row},{col}] has a bomb below')
+            neighbours+=1
+        if (col-1 >= 0) and ([row,col-1] in self.bombIndexes):   # check left
+            print(f'[{row},{col}] has a bomb to the left')
+            neighbours+=1
+        if (col+1 <= len(self.grid[0])) and ([row,col+1] in self.bombIndexes):   # check right
+            print(f'[{row},{col}] has a bomb to the right')
+            neighbours+=1
+        if (row-1 >= 0) and (col-1 >= 0) and ([row-1,col-1] in self.bombIndexes):  # check top left
+            print(f'[{row},{col}] has a bomb to the top left')
+            neighbours+=1
+        if (row-1 >= 0) and (col+1 <= len(self.grid[0])) and ([row-1,col+1] in self.bombIndexes):  # check top right
+            print(f'[{row},{col}] has a bomb to the top right')
+            neighbours+=1
+        if (row+1 <= len(self.grid)) and (col-1 >= 0) and ([row+1,col-1] in self.bombIndexes):  # check bottom left
+            print(f'[{row},{col}] has a bomb to the bottom left')
+            neighbours+=1
+        if (row+1 <= len(self.grid)) and (col+1 <= len(self.grid[0])) and ([row+1,col+1] in self.bombIndexes):  # check top right
+            print(f'[{row},{col}] has a bomb to the bottom right')
+            neighbours+=1
+        return neighbours
